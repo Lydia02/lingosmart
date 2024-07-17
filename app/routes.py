bee1ad3aa0cd3d96c36842eb4b78a19157545252
O-keita <o.keita@alustudent.com>
2024-07-17 15:50:14 +0000

    flask setup

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,35 @@
 from app import app
-from flask import render_template
+from flask import render_template, jsonify
 
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
     return render_template("home.html")
+
+
+
+
+
+
+
+
+
+
+# APIs
+#dummy
+users = [
+    {"id": 1, "name": "Alice", "email": "alice@example.com"},
+    {"id": 2, "name": "Bob", "email": "bob@example.com"}
+]
+@app.route("/api/users", methods=['GET', 'POST'])
+def get_users():
+    #dummy for now
+    return jsonify(users)
+
+@app.route("/api/users/<int:id>", methods=['GET', 'POST'])
+def get_user(id):
+    for user in users:
+        if user["id"] == id:
+            return jsonify(user)
+    
+    
