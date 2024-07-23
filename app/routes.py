c3494b604a88211e470d075ac7fd2dda4068e480
O-keita <o.keita@alustudent.com>
2024-07-23 15:43:49 +0000

    static files added

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,13 @@ def home():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
+@app.route("/reset", methods=['GET', 'POST'])
+def reset():
+    return render_template("reset.html")
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    return render_template("signup.html")
 
 
 
