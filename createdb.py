b1b1cbbc4ce0b9a51d7ffd27158f5baa095290d4
O-keita <o.keita@alustudent.com>
2024-07-29 20:01:56 +0000

    mysql added

---
+++ createdb.py
@@ -0,0 +1,31 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+try:
+    my_db = mysql.connector.connect(
+        host="lingosmart.mysql.pythonanywhere-services.com",
+        user="lingosmart",
+        password="Oms@2002",
+        database="lingosmart$user"
+    )
+
+    my_cursor = my_db.cursor()
+
+    # Explicitly select the database
+    my_cursor.execute("USE Okeita$SmartWaste")
+
+    print("Connected to database 'Nzavote'")
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print("Error:", err.msg)
+
+finally:
+    if my_db.is_connected():
+        my_cursor.close()
+        my_db.close()
+        print("MySQL connection is closed")
